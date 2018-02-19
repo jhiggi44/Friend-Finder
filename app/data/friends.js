54e677690e52164b43006902ec1051e82d89c15f
jhiggi44 <jordandhiggins@gmail.com>
2018-02-19 12:42:44 -0700

    third try... heroku!

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,29 +9,29 @@ var friends = [
     photo: "https://cdn.shopify.com/s/files/1/0558/2081/products/Mitchell_DarthVader_FINAL_sm_1024x1024.jpg?v=1493912085",
     scores: [
       "5",
+      "5",
       "1",
-      "4",
-      "4",
       "5",
       "1",
       "2",
       "5",
-      "4",
-      "1"
+      "2",
+      "1",
+      "5"
     ]
   },
   {
     name: "Ben Kenobi",
     photo: "https://kmflett.files.wordpress.com/2017/05/corbynlook.jpg",
     scores: [
+      "1",
       "5",
       "1",
-      "4",
+      "1",
       "4",
       "5",
       "1",
-      "2",
-      "5",
+      "3",
       "4",
       "1"
     ]
@@ -40,16 +40,16 @@ var friends = [
     name: "Luke Skywalker",
     photo: "https://img.thedailybeast.com/image/upload/v1492185166/articles/2015/03/26/isis-hasn-t-seized-luke-skywalker-s-farm-star-wars-geeks-who-rebuilt-it-angered-by-reports/150326-swin-isis-tatooine-tease_aoino9.jpg",
     scores: [
-      "3",
+      "4",
+      "4",
       "2",
       "1",
-      "3",
-      "2",
+      "4",
+      "5",
       "1",
-      "3",
-      "2",
-      "1",
-      "5"
+      "4",
+      "4",
+      "2"
     ]
   },
   {
@@ -59,13 +59,13 @@ var friends = [
       "3",
       "2",
       "1",
-      "3",
-      "2",
       "1",
-      "3",
-      "2",
+      "4",
+      "5",
       "1",
-      "4"
+      "5",
+      "4",
+      "1"
     ]
   },
   {
@@ -73,31 +73,31 @@ var friends = [
     photo: "https://media1.popsugar-assets.com/files/thumbor/T6xO6cMRSk8qrlfrY3FSZ4IPF64/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/17/918/n/1922283/3e46b7c959e66fedce4014.13690181_edit_img_cover_file_44154628_1508273585/i/Funny-Reactions-New-Star-Wars-Han-Solo-Film-Title.jpg",
     scores: [
       "5",
-      "4",
-      "3",
-      "2",
       "1",
       "5",
-      "4",
+      "2",
+      "3",
       "3",
       "2",
-      "1"
+      "3",
+      "5",
+      "3"
     ]
   },
   {
     name: "Gran Moff Tarkin",
     photo: "https://vignette.wikia.nocookie.net/starwars/images/c/c1/Tarkin1.png/revision/latest?cb=20130304023051",
     scores: [
-      "5",
-      "4",
       "3",
-      "2",
+      "1",
+      "4",
+      "5",
+      "1",
       "1",
       "5",
-      "4",
-      "3",
+      "1",
       "2",
-      "1"
+      "5"
     ]
   }
 ]
--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var app = express();
 // Sets initial port.
 var PORT = process.env.PORT || 8080;
 
+// Expose the public directory to access other files
+app.use(express.static(path.join(__dirname, './app/public')));
+
 // BodyParser makes it possible for our server to interpret data sent to it.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
