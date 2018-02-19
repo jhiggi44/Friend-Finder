4c6c93ad77bb5be30983f1e30b8753eafad00455
jhiggi44 <jordandhiggins@gmail.com>
2018-02-19 11:43:52 -0700

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends
+// ===============================================================================
+
+var friends = [
+  {
+    name: "Darth Vader",
+    photo: "https://cdn.shopify.com/s/files/1/0558/2081/products/Mitchell_DarthVader_FINAL_sm_1024x1024.jpg?v=1493912085",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Ben Kenobi",
+    photo: "https://kmflett.files.wordpress.com/2017/05/corbynlook.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Luke Skywalker",
+    photo: "https://img.thedailybeast.com/image/upload/v1492185166/articles/2015/03/26/isis-hasn-t-seized-luke-skywalker-s-farm-star-wars-geeks-who-rebuilt-it-angered-by-reports/150326-swin-isis-tatooine-tease_aoino9.jpg",
+    scores: [
+      "3",
+      "2",
+      "1",
+      "3",
+      "2",
+      "1",
+      "3",
+      "2",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "Princess Leia",
+    photo: "http://i.pinimg.com/736x/fe/e8/66/fee8661151ba6a61d862c191051b64c0.jpg",
+    scores: [
+      "3",
+      "2",
+      "1",
+      "3",
+      "2",
+      "1",
+      "3",
+      "2",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Han Solo",
+    photo: "https://media1.popsugar-assets.com/files/thumbor/T6xO6cMRSk8qrlfrY3FSZ4IPF64/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2017/10/17/918/n/1922283/3e46b7c959e66fedce4014.13690181_edit_img_cover_file_44154628_1508273585/i/Funny-Reactions-New-Star-Wars-Han-Solo-Film-Title.jpg",
+    scores: [
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+    ]
+  },
+  {
+    name: "Gran Moff Tarkin",
+    photo: "https://vignette.wikia.nocookie.net/starwars/images/c/c1/Tarkin1.png/revision/latest?cb=20130304023051",
+    scores: [
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "5",
+      "4",
+      "3",
+      "2",
+      "1"
+    ]
+  }
+]
+
+//Export the array - makes it accessible to other files using require.
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,31 @@
+
+// DEPENDENCIES
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+// EXPRESS
+// creates an "express" server
+var app = express();
+
+// Sets initial port.
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+require(path.join(__dirname, '/app/routing/apiroutes.js'))(app);
+require(path.join(__dirname, '/app/routing/htmlRoutes.js'))(app);
+
+// LISTENER
+// The below code effectively "starts" our server
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
